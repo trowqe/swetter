5c2d43bd61e84e82030bf48881790b3b446b9e8d
trowqe <PoO31WZXL6en5V8>
2021-05-17 12:27:46 +0300

    change thymeleaf to mustache

--- src/main/java/com/example/swetter/GreetingController.java
+++ src/main/java/com/example/swetter/GreetingController.java
@@ -1,9 +1,10 @@
 package com.example.swetter;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
@@ -11,9 +12,16 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            @RequestParam(name="name2", required=false, defaultValue="World2") String name2,
-                           Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("name2", name2);
+                           Map<String, Object> model) {
+        model.put("name", name);
+        model.put("name2", name2);
         return "greeting";
+    }
+
+    @GetMapping("/")
+    public String main(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model) {
+        model.put("name", name);
+        return "main";
     }
 }
